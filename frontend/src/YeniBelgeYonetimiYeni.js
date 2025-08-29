9060232aac08f39365e4159ccaa063a289765ccb
Developer <developer@emergent.com>
2025-08-29 19:39:41 +0000

    auto-commit for 4a038288-7483-4b23-b4ca-dac6e5789390

--- frontend/src/YeniBelgeYonetimiYeni.js
+++ frontend/src/YeniBelgeYonetimiYeni.js
@@ -1531,12 +1531,13 @@ const YeniBelgeYonetimiYeni = ({ selectedClient: propSelectedClient }) => {
                         </div>
                         
                         <div className="flex space-x-2">
-                          <button
+                          {/* Görüntüle butonu geçici olarak gizlendi */}
+                          {/* <button
                             onClick={() => viewDocument(doc)}
                             className="flex-1 px-3 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 text-sm font-medium"
                           >
                             👁️ Görüntüle
-                          </button>
+                          </button> */}
                           <button
                             onClick={() => downloadDocument(doc)}
                             className="flex-1 px-3 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 text-sm font-medium"
