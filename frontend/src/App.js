1d3521e77c9db260a212c4116512e9080131cf0b
Developer <developer@emergent.com>
2025-09-03 20:17:11 +0000

    auto-commit for f6136996-dc1e-4c29-a907-cf5a3e694d0f

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5459,8 +5459,8 @@ const CarbonFootprint = () => {
                         <span className="font-semibold text-yellow-800">Elektrik</span>
                       </div>
                       <div className="text-right">
-                        <div className="text-lg font-bold text-yellow-900">{carbonData.total_emission_sources.electricity.toFixed(2)}</div>
-                        <div className="text-xs text-yellow-600">kg CO2</div>
+                        <div className="text-lg font-bold text-yellow-900">{(carbonData.total_emission_sources.electricity / 1000).toFixed(3)}</div>
+                        <div className="text-xs text-yellow-600">tCO2</div>
                       </div>
                     </div>
                   </div>
