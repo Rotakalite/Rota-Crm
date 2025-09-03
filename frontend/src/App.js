a25c63e94ccf880ac4887e41c64afbff581fd8f5
Developer <developer@emergent.com>
2025-09-03 20:52:34 +0000

    auto-commit for bdae4c95-e255-48d5-93f7-59723ebd3929

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5770,7 +5770,9 @@ const CarbonFootprint = () => {
                             (carbonData.total_emission_sources.r410a_gas || 0) + 
                             (carbonData.total_emission_sources.r32_gas || 0),
                             (carbonData.total_emission_sources.co2_fire || 0) + 
-                            (carbonData.total_emission_sources.fm200_fire || 0)
+                            (carbonData.total_emission_sources.fm200_fire || 0),
+                            carbonData.total_waste_co2 || 0,    // NEW: Waste emissions
+                            carbonData.total_hotel_co2 || 0     // NEW: Hotel emissions
                           ].filter(value => value > 0),
                           backgroundColor: [
                             '#FCD34D', // Elektrik - Sarı
