3e9d3bfad37447ce7cac7441bd837d26c7f6d4e9
Developer <developer@emergent.com>
2025-09-16 11:23:13 +0000

    auto-commit for 85d35f8e-dec8-4c14-8bea-81c6d38d59c8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8017,7 +8017,7 @@ const WasteManagement = ({ selectedClient: propSelectedClient }) => {
 
               <div className="flex justify-end gap-3 mt-6">
                 <button
-                  onClick={handleSubmitRecord}
+                  onClick={handleRecordSubmit}
                   disabled={loading}
                   className="bg-gradient-to-r from-green-500 to-green-600 text-white px-6 py-2 rounded-lg hover:from-green-600 hover:to-green-700 disabled:opacity-50 font-medium"
                 >
