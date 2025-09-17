4265c6f04e3b10fa59c797e48b81b7add60a80a0
Developer <developer@emergent.com>
2025-09-17 20:56:34 +0000

    auto-commit for 16f8f3d1-3bb9-4e7d-b4e5-a60da8050be2

--- backend/server.py
+++ backend/server.py
@@ -926,7 +926,7 @@ async def ultra_cors_middleware(request, call_next):
     response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH"
     response.headers["Access-Control-Allow-Headers"] = "Accept, Accept-Language, Content-Language, Content-Type, Authorization, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers, Cache-Control, Pragma, Expires, X-CSRF-Token"
     response.headers["Access-Control-Expose-Headers"] = "*"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Credentials"] = "false"
     response.headers["Access-Control-Max-Age"] = "86400"
     
     return response
