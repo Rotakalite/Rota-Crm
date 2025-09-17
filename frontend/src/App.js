b065814e21e4453515cb0a7eec89ed95bf97879a
Developer <developer@emergent.com>
2025-09-17 23:03:55 +0000

    auto-commit for 3992d650-0fbc-4745-b0e2-0e3c72ab49da

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1102,6 +1102,7 @@ const FrontOfficeManagement = ({ selectedClient: propSelectedClient }) => {
     adults: 1,
     children: 0,
     room_rate: 0,
+    payment_status: 'pending',
     booking_source: 'front_desk',
     special_requests: '',
     notes: ''
