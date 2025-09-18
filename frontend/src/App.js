d5503c8f918da4662c9499c8eac40f06284242a9
Developer <developer@emergent.com>
2025-09-18 12:06:49 +0000

    auto-commit for 3a48e604-4d3b-47f1-8a7c-50d530a99f9e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2144,7 +2144,14 @@ const FrontOfficeManagement = ({ selectedClient: propSelectedClient }) => {
                       <input
                         type="date"
                         value={newReservation.check_in_date}
-                        onChange={(e) => setNewReservation({...newReservation, check_in_date: e.target.value})}
+                        onChange={(e) => {
+                          const newValue = e.target.value;
+                          setNewReservation({...newReservation, check_in_date: newValue});
+                          // Update available rooms when date changes
+                          if (newValue && newReservation.check_out_date) {
+                            fetchAvailableRoomsForDates(newValue, newReservation.check_out_date);
+                          }
+                        }}
                         className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         required
                       />
